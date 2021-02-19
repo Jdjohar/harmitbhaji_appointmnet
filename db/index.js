634bb0fffa8ddba17c53589789ddd4df624ebd22
Jashandeep Singh <jdeep514@gmail.com>
2021-02-19 19:41:02 +0530

    db route

--- db/index.js
+++ db/index.js
@@ -1,5 +1,19 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool({
+  connectionString:process.env.DATABASE_URL
+})
+.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM business_appoint');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 module.exports = {
   query: (text, params) => pool.query(text, params),
 }
