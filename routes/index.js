bd6a57d1e730ece92c835adfd76d8160d993a892
Jashandeep Singh <jdeep514@gmail.com>
2021-03-04 14:59:01 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -303,9 +303,19 @@ res.status(200).json({
 
 // CREAT A BUSINESS
 router.post("/api/v1/business", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', '*');
 
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+ 
+ //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ 
+   // Pass to next layer of middleware
+   next();
   console.log(req.body);
 
   try{
