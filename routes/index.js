801a40dd2520619526379a766f68b5f3f057f71a
Jashandeep Singh <jdeep514@gmail.com>
2021-03-05 17:31:36 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ const ical = require('ical-generator');
 var router = express.Router();
 
 router.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -29,19 +29,19 @@ router.use((req, res, next) => {
 });
 
 
-// router.use(morgan("dev"));
-router.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "http://tachitools.herokuapp.com/")
-  // res.header("Access-Control-Allow-Credentials: true") 
-  res.header("Access-Control-Allow-Headers", 
-  "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-  // res.header("Access-Control-Max-Age", "1000")
-  if (req.method == "OPTIONS"){
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
-      return res.status(200).json({})
-  }
-  next()
-})
+// // router.use(morgan("dev"));
+// router.use((req, res, next)=>{
+//   res.header("Access-Control-Allow-Origin", "http://tachitools.herokuapp.com/")
+//   // res.header("Access-Control-Allow-Credentials: true") 
+//   res.header("Access-Control-Allow-Headers", 
+//   "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+//   // res.header("Access-Control-Max-Age", "1000")
+//   if (req.method == "OPTIONS"){
+//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+//       return res.status(200).json({})
+//   }
+//   next()
+// })
 
 router.use(cors());
 var corsOptions = {
