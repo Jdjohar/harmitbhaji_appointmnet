d8e402e8e96309a82eca0ac711a6da6fd34a4dd3
Jashandeep Singh <jdeep514@gmail.com>
2021-03-05 19:27:28 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,13 @@ const ical = require('ical-generator');
 
 
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // const pool = new Pool({
 //   connectionString:process.env.DATABASE_URL,
 //   ssl:{ rejectUnauthorized: false }
