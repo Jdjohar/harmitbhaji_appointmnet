8ad680dbaf84dfe4513e51756aad448dfd7f2dc8
Jashandeep Singh <jdeep514@gmail.com>
2021-03-05 18:01:06 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -30,19 +30,19 @@ var router = express.Router();
 // });
   
 
-// router.use(morgan("dev"));
-router.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "*")
-  // res.header("Access-Control-Allow-Credentials: true") 
-  res.header("Access-Control-Allow-Headers", 
-  "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-  // res.header("Access-Control-Max-Age", "1000")
-  if (req.method == "OPTIONS"){
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
-      return res.status(200).json({})
-  }
-  next()
-})
+// // router.use(morgan("dev"));
+// router.use((req, res, next)=>{
+//   res.header("Access-Control-Allow-Origin", "*")
+//   // res.header("Access-Control-Allow-Credentials: true") 
+//   res.header("Access-Control-Allow-Headers", 
+//   "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+//   // res.header("Access-Control-Max-Age", "1000")
+//   if (req.method == "OPTIONS"){
+//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+//       return res.status(200).json({})
+//   }
+//   next()
+// })
 
 router.use(cors());
 // var corsOptions = {
