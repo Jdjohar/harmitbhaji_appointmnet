9fd6c6530672dd637482c0ad3e50876ad2225988
Jashandeep Singh <jdeep514@gmail.com>
2021-03-05 17:55:36 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ var router = express.Router();
 
 // router.use(morgan("dev"));
 router.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "http://tachitools.herokuapp.com/")
+  res.header("Access-Control-Allow-Origin", "*")
   // res.header("Access-Control-Allow-Credentials: true") 
   res.header("Access-Control-Allow-Headers", 
   "Origin, X-Requested-With, Content-Type, Accept, Authorization")
