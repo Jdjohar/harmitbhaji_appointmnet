0ba6d295d47d7961389c7e14eabb61be44964a72
Jashandeep Singh <jdeep514@gmail.com>
2021-03-05 17:23:39 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,29 @@ const ical = require('ical-generator');
 
 var router = express.Router();
 
+router.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://tachitools.herokuapp.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
 // router.use(morgan("dev"));
+router.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "http://tachitools.herokuapp.com/")
+  // res.header("Access-Control-Allow-Credentials: true") 
+  res.header("Access-Control-Allow-Headers", 
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  // res.header("Access-Control-Max-Age", "1000")
+  if (req.method == "OPTIONS"){
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+      return res.status(200).json({})
+  }
+  next()
+})
 
 router.use(cors());
 var corsOptions = {
