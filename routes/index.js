64802c9269717cf8047352c4c984cd081a2feb75
Jdjohar <Jdeep514@gmail.com>
2022-12-20 21:50:50 +0530

    file update

--- routes/index.js
+++ routes/index.js
@@ -22,19 +22,6 @@ const { json } = require('body-parser');
 var router = express.Router();
 router.all(cors());
 
-// Add Access Control Allow Origin headers
-// router.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   if (req.method ==='OPTIONS'){
-//     res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
 
 
 /* GET home page. */
